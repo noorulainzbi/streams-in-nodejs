cb5823580d8389c68b7caafdf8934d8e558491c8
Noor Ul Ain <noorulainzbi@gmail.com>
2026-01-05 10:46:31 +0500

    add code to print on lakh nums with buffer

--- measureTimeEfficiencyWithStreams.js
+++ measureTimeEfficiencyWithStreams.js
@@ -1,4 +1,5 @@
 import fs from "fs";
+
 console.time();
 // for (let i = 1; i <= 5000; i++) {
 //   if (i == 1) {
@@ -57,3 +58,63 @@ console.time();
 // writeStream.on("finish", () => {
 //   console.timeEnd();
 // });
+
+// Time 550ms
+// const fd = fs.openSync("open.txt", "a");
+// for (let i = 1; i <= 100000; i++) {
+//   fs.writeSync(fd, `${i} `);
+// }
+// fs.closeSync(fd);
+// console.timeEnd();
+
+/************ Time 45ms  ****************/
+const fd = fs.openSync("num.txt", "a");
+const bufferSize = 16 * 1024;
+let buffer = Buffer.alloc(bufferSize);
+let offset = 0;
+let data = "";
+
+for (let i = 1; i <= 100000; i++) {
+  data = `${i} `;
+  offset += buffer.write(data, offset);
+  if (offset + data.length >= buffer.byteLength) {
+    fs.writeSync(fd, buffer.subarray(0, offset));
+    offset = 0;
+  }
+}
+if (offset > 0) {
+  fs.writeSync(fd, buffer.subarray(0, offset));
+  offset = 0;
+}
+fs.closeSync(fd);
+console.timeEnd();
+// for (let i = 1; i <= 10; i++) {
+//   if (data.length + offset >= buffer.byteLength) {
+//     fs.writeSync(fd, buffer.subarray(0, offset));
+//     offset = 0;
+//   } else {
+//     data = `${i} `;
+//     offset += buffer.write(data, offset);
+//   }
+// }
+
+// // write the remaining data
+// if (offset > 0) {
+//   fs.writeSync(fd, buffer.subarray(0, offset));
+//   offset = 0;
+// }
+// // console.log(data);
+
+// fs.closeSync(fd);
+// console.timeEnd();
+
+/*
+let b1 = Buffer.alloc(12);
+let off = b1.write("Hello", 0);
+off += b1.write("World", 5);
+console.log(off);
+
+console.log(b1);
+console.log(b1.byteLength);
+console.log(off + "12".length >= b1.byteLength);
+*/
--- readingFileWithFileDescriptor.js
+++ readingFileWithFileDescriptor.js
@@ -1,13 +1,5 @@
 import fs from "fs";
 
-// Time 550ms
-// console.time();
-// const fd = fs.openSync("open.txt", "a");
-// for (let i = 1; i <= 100000; i++) {
-//   fs.writeSync(fd, `${i} `);
-// }
-// fs.closeSync(fd);
-// console.timeEnd();
 const fd = fs.openSync("open.txt");
 const buffer = Buffer.alloc(10);
 fs.read(
